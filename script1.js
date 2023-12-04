b3a7a884ec1544a09a614f82eb94ef59dc5e0ad5
Shkahmad <ahmadbahi4145@gmail.com>
2023-12-03 21:31:17 -0800

    first commit

---
+++ script1.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $(".menuicon").click(function () {
+      $(".navb").slideToggle();
+    });
+  });
+$(document).ready(function () {
+    $(".pages").click(function () {
+      $(".g_1").slideToggle(1000);
+    });
+  });
+
+    // $(document).ready(function () {
+    //         $(".pages").click(function () {
+    //             $(".g_1").show();
+    //           });
+              
+    //       });
+
+    
