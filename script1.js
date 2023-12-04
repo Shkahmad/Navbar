6f4cbf3a6fd3e9ef5b5fe9c50fc3cdbc532de0f3
Shkahmad <ahmadbahi4145@gmail.com>
2023-12-03 21:39:10 -0800

    hello

--- script1.js
+++ script1.js
@@ -1,19 +1,3 @@
-$(document).ready(function () {
-    $(".menuicon").click(function () {
-      $(".navb").slideToggle();
-    });
-  });
-$(document).ready(function () {
-    $(".pages").click(function () {
-      $(".g_1").slideToggle(1000);
-    });
-  });
 
-    // $(document).ready(function () {
-    //         $(".pages").click(function () {
-    //             $(".g_1").show();
-    //           });
-              
-    //       });
 
     
